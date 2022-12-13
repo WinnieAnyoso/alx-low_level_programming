d80be749d8860ff719eac1149af796b63fac6c6e
WinnieAnyoso <anyosow@gmail.com>
2022-12-13 12:02:40 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@ int main(void)
 /**
  * main - Entry point
  *
- * Return: always 0
+ * Return: always 0 (success)
  */
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+		return (0);
 }
