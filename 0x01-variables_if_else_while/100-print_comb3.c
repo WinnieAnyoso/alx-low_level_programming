9819c0ec533123698fc8ddf77da645cae0f9db3e
WinnieAnyoso <anyosow@gmail.com>
2022-12-13 20:30:21 -0800

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 	int i;
 	int j;
 
-	for (i=10; i<=19; i++)
+	for (i = 10; i <= 19; i++)
 	{
-		for (j =10; j <= 19; j++)
+		for (j = 10; j <= 19; j++)
 		{
-			putchar((i%10) + '0');
-			putchar((j%10) + '0');
-			if (i != 19 || j!=19)
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 19 || j!= 19)
 			{
 				putchar(',');
 				putchar(' ');
