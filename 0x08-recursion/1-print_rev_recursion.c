435cf53baa940b0cd9bc936ea0cdd534bdc93bbe
WinnieAnyoso <anyosow@gmail.com>
2023-01-06 09:28:09 -0800

    Rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Winnie Anyoso
+ */
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @a: The string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if(*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
